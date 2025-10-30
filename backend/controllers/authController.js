957ce63dcf2bfdb96240996214c0343d20815c92
Saloni9989 <155825936+Saloni9989@users.noreply.github.com>
2025-10-30 15:28:36 +0530

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { username, password, role } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword, role });
+  await user.save();
+  res.json({ message: 'User registered successfully' });
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ message: 'User not found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
+  res.json({ token, role: user.role });
+};
